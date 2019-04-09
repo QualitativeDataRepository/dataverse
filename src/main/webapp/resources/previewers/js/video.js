91ca16bd606f45d25cc70a7c32cd02ab3075402f
Jim Myers <myersjd@umich.edu>
2019-04-09 09:27:01 -0400

    remove local previewers

--- src/main/webapp/resources/previewers/js/video.js
+++
@@ -1,4 +0,0 @@
-function writeContent(fileUrl, label, creationDate, title, authors, parentUrl) {
-addStandardPreviewHeader(label, creationDate, title, authors, parentUrl);
-          $(".preview").append($("<video/>").attr("controls","").attr("src",fileUrl));
-}
