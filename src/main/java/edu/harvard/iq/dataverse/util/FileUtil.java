a92f5a34fa8e5a1f4282f63d453a9f30ca0524ff
Jim Myers <myersjd@umich.edu>
2022-03-29 20:47:12 -0400

    fix return - not clear why Eclipse didn't flag this

--- src/main/java/edu/harvard/iq/dataverse/util/FileUtil.java
+++ src/main/java/edu/harvard/iq/dataverse/util/FileUtil.java
@@ -1357,9 +1357,8 @@ public class FileUtil implements java.io.Serializable  {
                 logger.fine("Popup required because of terms of access.");
                 answer = true;
             }
-            return answer;
-
         }
+        return answer;
     }
 
     /**
