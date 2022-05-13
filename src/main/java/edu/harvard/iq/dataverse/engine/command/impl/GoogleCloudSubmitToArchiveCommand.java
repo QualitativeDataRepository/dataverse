b2fddb24f387f825dfbe138e8e2052ad076bf8fd
Jim Myers <myersjd@umich.edu>
2022-05-13 14:00:06 -0400

    Cherry pick refactor of archiver classes

--- src/main/java/edu/harvard/iq/dataverse/engine/command/impl/GoogleCloudSubmitToArchiveCommand.java
+++ src/main/java/edu/harvard/iq/dataverse/engine/command/impl/GoogleCloudSubmitToArchiveCommand.java
@@ -10,21 +10,16 @@ import edu.harvard.iq.dataverse.authorization.users.ApiToken;
 import edu.harvard.iq.dataverse.engine.command.Command;
 import edu.harvard.iq.dataverse.engine.command.DataverseRequest;
 import edu.harvard.iq.dataverse.engine.command.RequiredPermissions;
-import edu.harvard.iq.dataverse.util.bagit.BagGenerator;
-import edu.harvard.iq.dataverse.util.bagit.OREMap;
 import edu.harvard.iq.dataverse.workflow.step.Failure;
 import edu.harvard.iq.dataverse.workflow.step.WorkflowStepResult;
 
-import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.nio.charset.Charset;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -42,8 +37,6 @@ public class GoogleCloudSubmitToArchiveCommand extends AbstractSubmitToArchiveCo
     private static final Logger logger = Logger.getLogger(GoogleCloudSubmitToArchiveCommand.class.getName());
     private static final String GOOGLECLOUD_BUCKET = ":GoogleCloudBucket";
     private static final String GOOGLECLOUD_PROJECT = ":GoogleCloudProject";
-
-    boolean success = false;
 
     public GoogleCloudSubmitToArchiveCommand(DataverseRequest aRequest, DatasetVersion version) {
         super(aRequest, version);
@@ -77,7 +70,8 @@ public class GoogleCloudSubmitToArchiveCommand extends AbstractSubmitToArchiveCo
                     String dataciteXml = DOIDataCiteRegisterService.getMetadataFromDvObject(
                             dv.getDataset().getGlobalId().asString(), metadata, dv.getDataset());
                     MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-                    try (PipedInputStream dataciteIn = new PipedInputStream(); DigestInputStream digestInputStream = new DigestInputStream(dataciteIn, messageDigest)) {
+                    try (PipedInputStream dataciteIn = new PipedInputStream();
+                            DigestInputStream digestInputStream = new DigestInputStream(dataciteIn, messageDigest)) {
                         // Add datacite.xml file
 
                         Thread dcThread = new Thread(new Runnable() {
@@ -96,7 +90,8 @@ public class GoogleCloudSubmitToArchiveCommand extends AbstractSubmitToArchiveCo
                             }
                         });
                         dcThread.start();
-                        // Have seen broken pipe in PostPublishDataset workflow without this delay
+                        // Have seen Pipe Closed errors for other archivers when used as a workflow
+                        // without this delay loop
                         int i = 0;
                         while (digestInputStream.available() <= 0 && i < 100) {
                             Thread.sleep(10);
@@ -109,6 +104,7 @@ public class GoogleCloudSubmitToArchiveCommand extends AbstractSubmitToArchiveCo
                         logger.fine("Content: datacite.xml added with checksum: " + checksum);
                         String localchecksum = Hex.encodeHexString(digestInputStream.getMessageDigest().digest());
                         if (!success || !checksum.equals(localchecksum)) {
+                            logger.severe("Failure on " + spaceName);
                             logger.severe(success ? checksum + " not equal to " + localchecksum : "datacite.xml transfer did not succeed");
                             try {
                                 dcXml.delete(Blob.BlobSourceOption.generationMatch());
@@ -127,78 +123,22 @@ public class GoogleCloudSubmitToArchiveCommand extends AbstractSubmitToArchiveCo
                         // Google uses MD5 as one way to verify the
                         // transfer
                         messageDigest = MessageDigest.getInstance("MD5");
-                        try (PipedInputStream in = new PipedInputStream(100000); DigestInputStream digestInputStream2 = new DigestInputStream(in, messageDigest);) {
-                            Thread writeThread = new Thread(new Runnable() {
-                                public void run() {
-                                    try (PipedOutputStream out = new PipedOutputStream(in)) {
-                                        // Generate bag
-                                        BagGenerator bagger = new BagGenerator(new OREMap(dv, false), dataciteXml);
-                                        bagger.setAuthenticationKey(token.getTokenString());
-                                        bagger.generateBag(out);
-                                        success=true;
-                                    } catch (Exception e) {
-                                        logger.severe("Error creating bag: " + e.getMessage());
-                                        // TODO Auto-generated catch block
-                                        e.printStackTrace();
-                                        try {
-                                            digestInputStream2.close();
-                                        } catch (Exception ex) {
-                                            logger.warning(ex.getLocalizedMessage());
-                                        }
-                                        throw new RuntimeException("Error creating bag: " + e.getMessage());
-                                    }
-                                }
-                            });
-                            writeThread.start();
-                            /*
-                             * The following loop handles two issues. First, with no delay, the
-                             * bucket.create() call below can get started before the piped streams are set
-                             * up, causing a failure (seen when triggered in a PostPublishDataset workflow).
-                             * A minimal initial wait, e.g. until some bytes are available, would address
-                             * this. Second, the BagGenerator class, due to it's use of parallel streaming
-                             * creation of the zip file, has the characteristic that it makes a few bytes
-                             * available - from setting up the directory structure for the zip file -
-                             * significantly earlier than it is ready to stream file content (e.g. for
-                             * thousands of files and GB of content). If, for these large datasets,
-                             * bucket.create() is called as soon as bytes are available, the call can
-                             * timeout before the bytes for all the zipped files are available. To manage
-                             * this, the loop waits until 90K bytes are available, larger than any expected
-                             * dir structure for the zip and implying that the main zipped content is
-                             * available, or until the thread terminates, with all of its content written to
-                             * the pipe. (Note the PipedInputStream buffer is set at 100K above - I didn't
-                             * want to test whether that means that exactly 100K bytes will be available()
-                             * for large datasets or not, so the test below is at 90K.)
-                             * 
-                             * An additional sanity check limits the wait to 2K seconds. The BagGenerator
-                             * has been used to archive >120K files, 2K directories, and ~600GB files on the
-                             * SEAD project (streaming content to disk rather than over an internet
-                             * connection) which would take longer than 2K seconds (10+ hours) and might
-                             * produce an initial set of bytes for directories > 90K. If Dataverse ever
-                             * needs to support datasets of this size, the numbers here would need to be
-                             * increased, and/or a change in how archives are sent to google (e.g. as
-                             * multiple blobs that get aggregated) would be required.
-                             */
-                            i = 0;
-                            while (digestInputStream2.available() <= 90000 && i < 2000 && writeThread.isAlive()) {
-                                Thread.sleep(1000);
-                                logger.fine("avail: " + digestInputStream2.available() + " : " + writeThread.getState().toString());
-                                i++;
-                            }
-                            logger.fine("Bag: transfer started, i=" + i + ", avail = " + digestInputStream2.available());
-                            if (i == 2000) {
-                                throw new IOException("Stream not available");
-                            }
-                            Blob bag = bucket.create(spaceName + "/" + fileName, digestInputStream2, "application/zip", Bucket.BlobWriteOption.doesNotExist());
+                        try (PipedInputStream in = new PipedInputStream(100000);
+                                DigestInputStream digestInputStream2 = new DigestInputStream(in, messageDigest)) {
+                            Thread bagThread = startBagThread(dv, in, digestInputStream2, dataciteXml, token);
+                            Blob bag = bucket.create(spaceName + "/" + fileName, digestInputStream2, "application/zip",
+                                    Bucket.BlobWriteOption.doesNotExist());
                             if (bag.getSize() == 0) {
                                 throw new IOException("Empty Bag");
                             }
-                            writeThread.join();
+                            bagThread.join();
 
                             checksum = bag.getMd5ToHexString();
                             logger.fine("Bag: " + fileName + " added with checksum: " + checksum);
                             localchecksum = Hex.encodeHexString(digestInputStream2.getMessageDigest().digest());
                             if (!success || !checksum.equals(localchecksum)) {
-                                logger.severe(success ? checksum + " not equal to " + localchecksum : "bag transfer did not succeed");
+                                logger.severe(success ? checksum + " not equal to " + localchecksum
+                                        : "bag transfer did not succeed");
                                 try {
                                     bag.delete(Blob.BlobSourceOption.generationMatch());
                                 } catch (StorageException se) {
