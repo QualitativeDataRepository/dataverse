19f663984a94d40e093319d5f5d3ee5a65c34569
Jim Myers <qqmyers@hotmail.com>
2023-05-05 18:22:10 -0400

    debugging ft indexing fail

--- src/main/java/edu/harvard/iq/dataverse/search/IndexServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/search/IndexServiceBean.java
@@ -1112,6 +1112,7 @@ public class IndexServiceBean {
                     }
                     
                     /* Full-text indexing using Apache Tika */
+                    try {
                     if (doFullTextIndexing) {
                         if (!dataset.isHarvested() && !fileMetadata.getDataFile().isFilePackage()&& fileMetadata.getDataFile().getFilesize()!=0) {
                             StorageIO<DataFile> accessObject = null;
@@ -1151,7 +1152,6 @@ public class IndexServiceBean {
                                 e.printStackTrace();
                                 continue;
                             } catch (OutOfMemoryError e) {
-                                textHandler = null;
                                 logger.warning(String.format("Full-text indexing for %s failed due to OutOfMemoryError",
                                         fileMetadata.getDataFile().getDisplayName()));
                                 continue;
@@ -1162,11 +1162,17 @@ public class IndexServiceBean {
                                         fileMetadata.getDataFile().getDisplayName(),e.getClass().getCanonicalName(), e.getLocalizedMessage()));
                                 continue;
                             } finally {
+                                textHandler = null;
                                 accessObject.closeInputStream();
                             }
                         }
                     }
-
+                    } catch (Throwable e) {
+                            logger.warning(String.format("Throwable Full-text indexing for %s failed",
+                                fileMetadata.getDataFile().getDisplayName()));
+                        e.printStackTrace();
+                    }
+                    logger.fine("Continuing with file: " + fileMetadata.getDataFile().getId());
                     String filenameCompleteFinal = "";
                     if (fileMetadata != null) {
                         String filenameComplete = fileMetadata.getLabel();
