7a321d7cc5855f58857f5870ec48d4b66af1bb56
Jim Myers <qqmyers@hotmail.com>
2023-06-01 15:09:25 -0400

    fix non-merge-able error when recording thumb fail

--- src/main/java/edu/harvard/iq/dataverse/DataFileServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/DataFileServiceBean.java
@@ -1145,15 +1145,15 @@ public class DataFileServiceBean implements java.io.Serializable {
         */
                 
         
-       if (ImageThumbConverter.isThumbnailAvailable(file)) {
-           file = this.find(file.getId());
-           file.setPreviewImageAvailable(true);
-           this.save(file); 
-           return true;
-       }
-       file.setPreviewsHaveFailed(true);
-       this.save(file);
-       return false;
+        file = this.find(file.getId());
+        if (ImageThumbConverter.isThumbnailAvailable(file)) {
+            file.setPreviewImageAvailable(true);
+            this.save(file);
+            return true;
+        }
+        file.setPreviewsHaveFailed(true);
+        this.save(file);
+        return false;
     }
 
     
