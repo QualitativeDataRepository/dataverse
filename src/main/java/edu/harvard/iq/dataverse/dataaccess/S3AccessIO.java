ed8f358dd0f14840c4696f125c8eb1d618902070
Jim Myers <qqmyers@hotmail.com>
2023-10-06 12:13:19 -0400

    remove broken/inefficient bucket check

--- src/main/java/edu/harvard/iq/dataverse/dataaccess/S3AccessIO.java
+++ src/main/java/edu/harvard/iq/dataverse/dataaccess/S3AccessIO.java
@@ -111,28 +111,14 @@ public class S3AccessIO<T extends DvObject> extends StorageIO<T> {
             if(!StringUtil.isEmpty(proxy)&&StringUtil.isEmpty(endpoint)) {
                 logger.severe(driverId + " config error: Must specify a custom-endpoint-url if proxy-url is specified");
             }
-            //Not sure this is needed but moving it from the open method for now since it definitely doesn't need to run every time an object is opened.
-            try {
-/*                if (bucketName == null || !s3.doesBucketExistV2(bucketName)) {
-                    throw new IOException("ERROR: S3AccessIO - You must create and configure a bucket before creating datasets.");
-                }
-                */
-            } catch (SdkClientException sce) {
-                logger.warning(sce.getMessage());
-                if(sce.getCause()!=null) {
-                    logger.warning("Cause: " + sce.getCause().getLocalizedMessage());
-                }
-                errCount=(errCount+1)%100;
-                if(errCount==1) {
-                  sce.printStackTrace();
-                  logger.info("driverId: " + driverId);
-                  logger.info("bucketName: " + bucketName);
-                  logger.info("endpoint: " + endpoint);
-                  logger.info("proxy: " + proxy);
-                  logger.info("profile: " + System.getProperty("dataverse.files." + driverId + ".profile"));
-                }
-                throw new IOException("ERROR: S3AccessIO - Failed to look up bucket "+bucketName+" (is AWS properly configured?): " + sce.getMessage());
-            }
+            
+            // FWIW: There used to be a check here to see if the bucket exists. It was
+            // broken/didn't handle custom endpoints.
+            // It was very redundant (checking every time we access any file) and didn't do
+            // much but potentially make the failure (in the unlikely case a bucket doesn't
+            // exist/just disappeared) happen slightly earlier (here versus at the first
+            // file/metadata access).
+                    
         } catch (Exception e) {
             throw new AmazonClientException(
                         "Cannot instantiate a S3 client for " + bucketName + "; check your AWS credentials and region",
