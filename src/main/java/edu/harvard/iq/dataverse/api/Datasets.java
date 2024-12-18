a7734b5ab784fc375a41993539ded0356bf084d3
Jim Myers <qqmyers@hotmail.com>
2024-12-18 15:42:32 -0500

    merge fix

--- src/main/java/edu/harvard/iq/dataverse/api/Datasets.java
+++ src/main/java/edu/harvard/iq/dataverse/api/Datasets.java
@@ -5315,50 +5315,6 @@ public class Datasets extends AbstractApiBean {
             return error(BAD_REQUEST, ex.getMessage());
         }
     }
-    
-    @PUT
-    @AuthRequired
-    @Path("{id}/versions/{versionId}/versionNote")
-    public Response addVersionCreationNote(@Context ContainerRequestContext crc, @PathParam("id") String datasetId, @PathParam("versionId") String versionId, String note, @Context UriInfo uriInfo, @Context HttpHeaders headers) throws WrappedResponse {
-        if(!FeatureFlags.VERSION_CREATION_NOTE.enabled()) {
-            return notFound(BundleUtil.getStringFromBundle("datasets.api.addCreationNote.notEnabled")); 
-        }
-        if (!DS_VERSION_DRAFT.equals(versionId)) {
-            AuthenticatedUser user = getRequestAuthenticatedUserOrDie(crc);
-            if (!user.isSuperuser()) {
-                return forbidden(BundleUtil.getStringFromBundle("datasets.api.addCreationNote.forbidden"));
-            }
-        }
-        return response(req -> {
-            DatasetVersion datasetVersion = getDatasetVersionOrDie(req, versionId, findDatasetOrDie(datasetId), uriInfo, headers);
-            datasetVersion.setCreationNote(note);
-            execCommand(new UpdateDatasetVersionCommand(datasetVersion.getDataset(), req));
-
-            return ok("Note added");
-        }, getRequestUser(crc));
-    }
-    
-    @DELETE
-    @AuthRequired
-    @Path("{id}/versions/{versionId}/versionNote")
-    public Response deleteVersionCreationNote(@Context ContainerRequestContext crc, @PathParam("id") String datasetId, @PathParam("versionId") String versionId, @Context UriInfo uriInfo, @Context HttpHeaders headers) throws WrappedResponse {
-        if(!FeatureFlags.VERSION_CREATION_NOTE.enabled()) {
-            return notFound(BundleUtil.getStringFromBundle("datasets.api.addCreationNote.notEnabled")); 
-        }
-        if (!DS_VERSION_DRAFT.equals(versionId)) {
-            AuthenticatedUser user = getRequestAuthenticatedUserOrDie(crc);
-            if (!user.isSuperuser()) {
-                return forbidden(BundleUtil.getStringFromBundle("datasets.api.addCreationNote.forbidden"));
-            }
-        }
-        return response(req -> {
-            DatasetVersion datasetVersion = getDatasetVersionOrDie(req, versionId, findDatasetOrDie(datasetId), uriInfo, headers);
-            datasetVersion.setCreationNote(null);
-            execCommand(new UpdateDatasetVersionCommand(datasetVersion.getDataset(), req));
-
-            return ok("Note deleted");
-        }, getRequestUser(crc));
-    }
 
     @GET
     @AuthRequired
