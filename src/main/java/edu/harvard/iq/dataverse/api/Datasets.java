32e64838bf5bb6915f1be5f4d019fea2403f7ef0
Jim Myers <qqmyers@hotmail.com>
2025-02-14 09:55:47 -0500

    Initial refactor

--- src/main/java/edu/harvard/iq/dataverse/api/Datasets.java
+++ src/main/java/edu/harvard/iq/dataverse/api/Datasets.java
@@ -68,6 +68,7 @@ import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.core.*;
 import jakarta.ws.rs.core.Response.Status;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.util.Strings;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
@@ -4662,14 +4663,15 @@ public class Datasets extends AbstractApiBean {
                 }
                 DatasetVersion dsv = dataset.getLatestVersion();
                 String name = dataset.getCurrentName().replace("\"", "\"\"");
-                String status = dsv.getExternalStatusLabel();
+                CurationStatus status = dsv.getCurrentCurationStatus();
+                String label = (status!=null && Strings.isNotBlank(status.getLabel())) ? status.getLabel(): null;
                 String url = systemConfig.getDataverseSiteUrl() + dataset.getTargetUrl() + dataset.getGlobalId().asString();
                 String date = new SimpleDateFormat("yyyy-MM-dd").format(dsv.getCreateTime());
                 String modDate = new SimpleDateFormat("yyyy-MM-dd").format(dsv.getLastUpdateTime());
                 String hyperlink = "\"=HYPERLINK(\"\"" + url + "\"\",\"\"" + name + "\"\")\"";
                 List<String> sList = new ArrayList<String>();
                 assignees.entrySet().forEach(e -> sList.add(e.getValue().size() == 0 ? "" : String.join(";", e.getValue())));
-                csvSB.append("\n").append(String.join(",", hyperlink, date, modDate, status == null ? "" : status, String.join(",", sList)));
+                csvSB.append("\n").append(String.join(",", hyperlink, date, modDate, (status == null) ? "" : label, String.join(",", sList)));
             }
         }
         csvSB.append("\n");
