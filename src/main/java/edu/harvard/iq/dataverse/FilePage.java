475f5b3434b23850ad176a329282059b0e962d56
Jim Myers <qqmyers@hotmail.com>
2025-02-15 15:56:45 -0500

    add option to let creators see curation status

--- src/main/java/edu/harvard/iq/dataverse/FilePage.java
+++ src/main/java/edu/harvard/iq/dataverse/FilePage.java
@@ -357,6 +357,10 @@ public class FilePage implements java.io.Serializable {
     public boolean canPublishDataset(){
         return permissionsWrapper.canIssuePublishDatasetCommand(fileMetadata.getDatasetVersion().getDataset());
     }
+    
+    public boolean canSeeCurationStatus(){
+        return permissionsWrapper.canSeeCurationStatus(fileMetadata.getDatasetVersion().getDataset());
+    }
    
 
     public FileMetadata getFileMetadata() {
