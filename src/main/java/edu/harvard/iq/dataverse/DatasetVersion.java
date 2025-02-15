e9ba9ee60daf5f64ba17cd16f784e3aa582a41e3
Jim Myers <qqmyers@hotmail.com>
2025-02-15 16:59:30 -0500

    fix #10835

--- src/main/java/edu/harvard/iq/dataverse/DatasetVersion.java
+++ src/main/java/edu/harvard/iq/dataverse/DatasetVersion.java
@@ -2185,4 +2185,20 @@ public class DatasetVersion implements Serializable {
     public void setCreationNote(String creationNote) {
         this.creationNote = creationNote;
     }
+    public CurationStatus getCurationStatusAsOfDate(Date date) {
+        if (curationStatuses == null || curationStatuses.isEmpty()) {
+            return null;
+        }
+
+        // Find the first status whose createTime is before or equal to the given date
+        for (CurationStatus status : curationStatuses) {
+            if (status.getCreateTime().compareTo(date) <= 0) {
+                return status;
+            }
+        }
+
+        // If no status is found before the given date, return null
+        return null;
+    }
+
 }
