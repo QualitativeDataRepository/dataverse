d56de1321e71cc5ea289d0bad57b7db0f02d33d3
Jim Myers <qqmyers@hotmail.com>
2025-03-31 18:13:57 -0400

    drop String.class

--- src/main/java/edu/harvard/iq/dataverse/RoleAssignment.java
+++ src/main/java/edu/harvard/iq/dataverse/RoleAssignment.java
@@ -57,14 +57,13 @@ import jakarta.persistence.UniqueConstraint;
         query = " SELECT COUNT(r) FROM RoleAssignment r WHERE r.assigneeIdentifier=:assigneeIdentifier AND r.role.id IN (SELECT d.id FROM DataverseRole d WHERE d.alias IN ('admin', 'curator'))" )
 })
 @NamedNativeQuery(
-        name = "RoleAssignment.findAssigneesWithPermissionOnDvObject",
-        query = "SELECT DISTINCT ra.assigneeidentifier FROM roleassignment ra " +
-                "JOIN dataverserole dr ON ra.role_id = dr.id " +
-                "JOIN dvobject dob ON ra.definitionpoint_id = dob.id " +
-                "WHERE get_bit(dr.permissionbits::bit(64), :bitpos) = '1' " +
-                "AND dob.id = :objectId",
-        resultClass = String.class
-    )
+    name = "RoleAssignment.findAssigneesWithPermissionOnDvObject",
+    query = "SELECT DISTINCT ra.assigneeidentifier FROM roleassignment ra " +
+            "JOIN dataverserole dr ON ra.role_id = dr.id " +
+            "JOIN dvobject dob ON ra.definitionpoint_id = dob.id " +
+            "WHERE get_bit(dr.permissionbits::bit(64), :bitpos) = '1' " +
+            "AND dob.id = :objectId"
+))
 public class RoleAssignment implements java.io.Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
