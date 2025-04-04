1d84123a9a68419cf8959009bbd31f9a796c555c
Jim Myers <qqmyers@hotmail.com>
2025-04-04 11:51:08 -0400

    typo

--- src/main/java/edu/harvard/iq/dataverse/RoleAssignment.java
+++ src/main/java/edu/harvard/iq/dataverse/RoleAssignment.java
@@ -75,7 +75,7 @@ import jakarta.persistence.UniqueConstraint;
                     "    LEFT JOIN dataverse dv ON dvo.id = dv.id " +
                     "    WHERE dvo.id = ?2 " +
                     "    UNION ALL " +
-                    "    SELECT dvo.id, dvo.owner_id, dvo.permissionroot, oh.level + 1 " +
+                    "    SELECT dvo.id, dvo.owner_id, dv.permissionroot, oh.level + 1 " +
                     "    FROM dvobject dvo " +
                     "    LEFT JOIN dataverse dv ON dvo.id = dv.id " +
                     "    JOIN owner_hierarchy oh ON dvo.owner_id = oh.id " +
