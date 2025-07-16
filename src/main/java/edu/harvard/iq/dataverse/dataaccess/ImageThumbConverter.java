800524b128340447a85ebffe6d0b50217f9b65be
Jim Myers <qqmyers@hotmail.com>
2025-07-16 10:22:21 -0400

    Revert "Revert "use isPreviewImageAvailable flag""
    This reverts commit 48c1ad280c1cc21f7c9694afa602ffc796b4ae30.

--- src/main/java/edu/harvard/iq/dataverse/dataaccess/ImageThumbConverter.java
+++ src/main/java/edu/harvard/iq/dataverse/dataaccess/ImageThumbConverter.java
@@ -75,6 +75,9 @@ public class ImageThumbConverter {
 
     public static boolean isThumbnailAvailable(DataFile file) {
         logger.fine("isThumbnailAvailable called for " + file.getId());
+        if(file.isPreviewImageAvailable()) {
+            return true;
+        }
         return isThumbnailAvailable(file, DEFAULT_THUMBNAIL_SIZE);
     }
 
