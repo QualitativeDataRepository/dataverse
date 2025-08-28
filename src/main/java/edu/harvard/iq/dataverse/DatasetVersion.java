fc5a1e6e9acddec898e746c3165ab8dde7f1f335
Jim Myers <qqmyers@hotmail.com>
2025-08-28 09:54:28 -0400

    fix differences from v6.7.1

--- src/main/java/edu/harvard/iq/dataverse/DatasetVersion.java
+++ src/main/java/edu/harvard/iq/dataverse/DatasetVersion.java
@@ -209,7 +209,7 @@ public class DatasetVersion implements Serializable {
     @Transient 
     private String jsonLd;
 
-    @OneToMany(mappedBy="datasetVersion", cascade={CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST})
+    @OneToMany(mappedBy="datasetVersion", orphanRemoval = true, cascade={CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST})
     private List<DatasetVersionUser> datasetVersionUsers;
     
     // Is this the right mapping and cascading for when the workflowcomments table is being used for objects other than DatasetVersion?
@@ -1373,16 +1373,14 @@ public class DatasetVersion implements Serializable {
                                 relatedPublication.setText(subField.getDisplayValue());
                                 break;
                             case DatasetFieldConstant.publicationURL:
-                                // We have to avoid using subField.getDisplayValue() here - because the
-                                // DisplayFormatType
-                                // for this url metadata field is likely set up so that the display value is
-                                // automatically
-                                // turned into a clickable HTML HREF block, which we don't want to end in our
-                                // Schema.org
-                                // JSON-LD output. So we want to use the raw value of the field instead, with
-                                // minimal HTML
-                                // sanitation, just in case (this would be done on all URLs in
-                                // getDisplayValue()).
+                                /*
+                                 * We have to avoid using subField.getDisplayValue() here - because the
+                                 * DisplayFormatType for this url metadata field is likely set up so that the
+                                 * display value is automatically turned into a clickable HTML HREF block, which
+                                 * we don't want to end in our Schema.org JSON-LD output. So we want to use the
+                                 * raw value of the field instead, with minimal HTML sanitation, just in case
+                                 * (this would be done on all URLs in getDisplayValue()).
+                                 */
                                 String url = subField.getValue();
                                 if (StringUtils.isBlank(url) || DatasetField.NA_VALUE.equals(url)) {
                                     relatedPublication.setUrl("");
