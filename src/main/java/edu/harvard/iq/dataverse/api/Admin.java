9f17a2f1e466e8a36d9dfda52551ee1e7d687f62
Jim Myers <qqmyers@hotmail.com>
2025-11-25 14:34:32 -0500

    doc that api doesn't handls supportsDelete yet

--- src/main/java/edu/harvard/iq/dataverse/api/Admin.java
+++ src/main/java/edu/harvard/iq/dataverse/api/Admin.java
@@ -2027,6 +2027,7 @@ public class Admin extends AbstractApiBean {
             if(dv==null) {
                 return error(Status.BAD_REQUEST, "Requested version not found.");
             }
+            //ToDo - allow forcing with a non-success status for archivers that supportsDelete()
             if (dv.getArchivalCopyLocation() == null) {
                 String className = settingsService.getValueForKey(SettingsServiceBean.Key.ArchiverClassName);
                 // Note - the user is being sent via the createDataverseRequest(au) call to the
@@ -2092,7 +2093,7 @@ public class Admin extends AbstractApiBean {
 
         try {
             AuthenticatedUser au = getRequestAuthenticatedUserOrDie(crc);
-
+            //ToDo - allow forcing with a non-success status for archivers that supportsDelete()
             List<DatasetVersion> dsl = datasetversionService.getUnarchivedDatasetVersions();
             if (dsl != null) {
                 if (listonly) {
