4fab33588856f2fd4b13f45d9ce67e3b311917b4
Jim Myers <qqmyers@hotmail.com>
2026-03-04 17:04:40 -0500

    refactor index/perms call

--- src/main/java/edu/harvard/iq/dataverse/api/Index.java
+++ src/main/java/edu/harvard/iq/dataverse/api/Index.java
@@ -373,8 +373,8 @@ public class Index extends AbstractApiBean {
     @GET
     @Path("perms")
     public Response indexAllPermissions() {
-        IndexResponse indexResponse = solrIndexService.indexAllPermissions();
-        return ok(indexResponse.getMessage());
+        solrIndexService.asyncIndexAllPermissions();
+        return ok("Asynchronous indexing of all permissions has been started. Check the server logs for progress.");
     }
 
     @GET
