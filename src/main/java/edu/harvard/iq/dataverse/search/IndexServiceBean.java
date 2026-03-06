36a7bf552081b8ce6f8f49dd5dc8d38c27c3758a
Jim Myers <qqmyers@hotmail.com>
2026-03-06 11:40:46 -0500

    fix async/new txtn for updatelastIndex

--- src/main/java/edu/harvard/iq/dataverse/search/IndexServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/search/IndexServiceBean.java
@@ -165,6 +165,9 @@ public class IndexServiceBean {
     @EJB
     DatasetFieldServiceBean datasetFieldService;
 
+    @EJB
+    IndexServiceBean self;
+    
     @Inject
     DatasetVersionFilesServiceBean datasetVersionFilesServiceBean;
 
@@ -502,7 +505,7 @@ public class IndexServiceBean {
 
     public void indexDataset(Dataset dataset, boolean doNormalSolrDocCleanUp) throws  SolrServerException, IOException {
         doIndexDataset(dataset, doNormalSolrDocCleanUp);
-        updateLastIndexedTime(dataset.getId());
+        self.updateLastIndexedTime(dataset.getId());
         logger.fine("indextime updated for dataset " + dataset.getId());
     }
     
@@ -1883,15 +1886,15 @@ public class IndexServiceBean {
     }
 
     @Asynchronous
-    private void updateLastIndexedTime(Long id) {
+    public void updateLastIndexedTime(Long id) {
         // indexing is often in a transaction with update statements
         // if we flush on query (flush-mode auto), we want to prevent locking
         // -> update the dataset asynchronously in a new transaction
-        updateLastIndexedTimeInNewTransaction(id);
+        self.updateLastIndexedTimeInNewTransaction(id);
     }
 
     @TransactionAttribute(REQUIRES_NEW)
-    private void updateLastIndexedTimeInNewTransaction(Long id) {
+    public void updateLastIndexedTimeInNewTransaction(Long id) {
         /// Dataset updatedDataset =
         /// (Dataset)dvObjectService.updateContentIndexTime(dataset);
         /// updatedDataset = null;
