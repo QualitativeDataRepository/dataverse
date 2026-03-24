d54e11933d070a084c4977be0fe1e509e42d7a5f
Jim Myers <qqmyers@hotmail.com>
2026-03-24 10:57:39 -0400

    CORS fix for ssoreset path

--- src/main/java/edu/harvard/iq/dataverse/authorization/AuthFilter.java
+++ src/main/java/edu/harvard/iq/dataverse/authorization/AuthFilter.java
@@ -92,6 +92,9 @@ public class AuthFilter implements Filter {
                 //After resetting, just return with no content
                 HttpServletResponse httpServletResponse = (HttpServletResponse) response;
                 httpServletResponse.setStatus(200);
+                // We must call doFilter to allow other filters (e.g. CORS) to run.
+                // However, we return immediately after to stop further processing for this path.
+                filterChain.doFilter(servletRequest, response);
                 return;
             }
             if ((httpServletRequest.getMethod() == HttpMethod.GET) && !isCheck && (path.equals("/") || path.endsWith(".xhtml") && !(path.endsWith("logout.xhtml")|| path.endsWith("privateurl.xhtml") || path.contains("jakarta.faces.resource") || path.contains("/oauth2/callback")))) {
