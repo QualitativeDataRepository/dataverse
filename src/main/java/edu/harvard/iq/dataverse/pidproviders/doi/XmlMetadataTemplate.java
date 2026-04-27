aff8a5959f2fec7b905047c60e1f4806f4421f91
Jim Myers <qqmyers@hotmail.com>
2026-04-27 14:07:16 -0400

    add valueURI from keyword to subject element

--- src/main/java/edu/harvard/iq/dataverse/pidproviders/doi/XmlMetadataTemplate.java
+++ src/main/java/edu/harvard/iq/dataverse/pidproviders/doi/XmlMetadataTemplate.java
@@ -344,6 +344,7 @@ public class XmlMetadataTemplate {
             String keyword = null;
             String scheme = null;
             String schemeUri = null;
+            String valueUri = null;
 
             for (DatasetField subField : keywordFieldValue.getChildDatasetFields()) {
                 switch (subField.getDatasetFieldType().getName()) {
@@ -356,6 +357,9 @@ public class XmlMetadataTemplate {
                 case DatasetFieldConstant.keywordVocabURI:
                     schemeUri = subField.getValue();
                     break;
+                case DatasetFieldConstant.keywordTermURI:
+                    valueUri = subField.getValue();
+                    break;
                 }
             }
             if (StringUtils.isNotBlank(keyword)) {
@@ -365,6 +369,9 @@ public class XmlMetadataTemplate {
                 }
                 if (StringUtils.isNotBlank(schemeUri)) {
                     attributesMap.put("schemeURI", schemeUri);
+                }
+                if (StringUtils.isNotBlank(valueUri)) {
+                    attributesMap.put("valueURI", valueUri);
                 }
                 subjectsCreated = XmlWriterUtil.writeOpenTagIfNeeded(xmlw, "subjects", subjectsCreated);
                 XmlWriterUtil.writeFullElementWithAttributes(xmlw, "subject", attributesMap, StringEscapeUtils.escapeXml10(keyword));
